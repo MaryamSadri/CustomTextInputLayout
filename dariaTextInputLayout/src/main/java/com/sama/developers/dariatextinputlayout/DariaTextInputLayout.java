6b610ef7afac77b27a660ad4840a244a56b90e3a
maryamsadri <msadri20@gmail.com>
2022-08-23 18:09:57 +0430

    DariaTextInputLayout: The TextInputEditText created.

--- dariaTextInputLayout/src/main/java/com/sama/developers/dariatextinputlayout/DariaTextInputLayout.java
+++ dariaTextInputLayout/src/main/java/com/sama/developers/dariatextinputlayout/DariaTextInputLayout.java
@@ -2,9 +2,13 @@ package com.sama.developers.dariatextinputlayout;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class DariaTextInputLayout extends TextInputLayout {
+
+    private TextInputEditText editText;
 
     public DariaTextInputLayout(Context context) {
         super(context);
@@ -12,10 +16,25 @@ public class DariaTextInputLayout extends TextInputLayout {
 
     public DariaTextInputLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs, com.google.android.material.R.style.ThemeOverlay_MaterialComponents_TextInputEditText);
     }
 
     public DariaTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
     }
 
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        setWillNotDraw(false);
+        editText = new TextInputEditText(getContext());
+        createEditBox(editText);
+    }
+
+
+    private void createEditBox(TextInputEditText editText) {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        editText.setPadding(30,50,30,50);
+        editText.setLayoutParams(layoutParams);
+        addView(editText);
+    }
 }
